b3183ac0120f05d56c59c7d495eb514436e6bab2
Khueeeee <129375443+Khueeeee@users.noreply.github.com>
2024-06-28 15:40:15 +0700

    upload code

---
+++ js/style(cúc ku).js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+var myCarousel = document.querySelector('#carouselExampleCaptions')
+var carousel = new bootstrap.Carousel(myCarousel,{
+})
